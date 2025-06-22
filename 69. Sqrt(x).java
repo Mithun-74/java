a1f0401923b1aa208afc23212f9b231427c58d2b
MITHUN <151951068+Mithun-74@users.noreply.github.com>
2025-06-22 23:31:18 +0530

    Create 69. Sqrt(x).java

---
+++ 69. Sqrt(x).java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int l = 1, r = x;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x)
+                return mid;
+            else if (square < x)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return r;
+    }
+}
